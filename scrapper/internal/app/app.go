4e82a2f32921c6d375553387cef6e99d10882709
anton <antonfedorov190@yandex.ru>
2025-03-16 19:47:32 +0300

    chore(logging): add logging

--- scrapper/internal/app/app.go
+++ scrapper/internal/app/app.go
@@ -10,15 +10,16 @@ import (
 	"syscall"
 	"tbank/scrapper/api/proto/gen"
 	"tbank/scrapper/config"
-	"tbank/scrapper/internal/closer"
 	grpcserver "tbank/scrapper/internal/controllers/grpc"
 	"tbank/scrapper/internal/gateway"
 	"tbank/scrapper/internal/hub"
 	kafkaproducer "tbank/scrapper/internal/kafka_producer"
+	"tbank/scrapper/internal/repository/postgres"
 	"tbank/scrapper/internal/service"
-	git "tbank/scrapper/pkg/github"
+	git "tbank/scrapper/pkg/github_client"
 
 	"github.com/IBM/sarama"
+	"github.com/lmittmann/tint"
 	"google.golang.org/grpc"
 )
 
@@ -29,23 +30,41 @@ type App struct {
 	logger     		*slog.Logger
 	kafkaProducer 	*kafkaproducer.KafkaProducer
 	hub				hub.Hub
-	closer			*closer.Closer
+	closer			*Closer
 }
 
 func NewApp() (*App, error) {
 
+	logger := slog.New(tint.NewHandler(os.Stdout, nil)).With(slog.String("SERVICE", "SCRAPPER"))
+
 	cfg, err := config.LoadConfig()
 	if err != nil {
+		logger.Error("Failed to laod the config", slog.String("error", err.Error()))
 		return nil, err
 	}
 
-	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	logger.Info("Successfully loaded the config")
 
-	closer := closer.NewCloser(logger)
+	pool, err := cfg.DataBase.ConnectToPostgres(context.Background())
+	if err != nil {
+		logger.Error("Failed to connect to DB", slog.String("error", err.Error()))
+		return nil, err
+	}
+
+	logger.Info("Successfully connected to PostgreSQL")
+
+	closer := NewCloser()
+
+	closer.Add(func(ctx context.Context) error {
+		logger.Info("Closing pgx.Pool...")
+		pool.Close()
+		return nil
+	})
 
 	grpcServer := grpc.NewServer()
 
-	closer.Add(func() error {
+	closer.Add(func(ctx context.Context) error {
+		logger.Info("Stopping gRPC serve...")
 		grpcServer.GracefulStop()
 		return nil
 	})
@@ -58,32 +77,41 @@ func NewApp() (*App, error) {
 	saramaConfig.Producer.Return.Successes = true
 	saramaConfig.Producer.Return.Errors = true
 
-	producer, err := sarama.NewAsyncProducer([]string{"localhost:9093"}, saramaConfig) 
+	producer, err := sarama.NewAsyncProducer(cfg.Kafka.Addresses, saramaConfig) 
 	if err != nil {
+		logger.Error("Failed to create a new async Kafka producer", slog.String("error", err.Error()))
 		return nil, err
 	}
-
-	kafkaTopic := cfg.Kafka.Topic
 	
-	kafkaProducer, err := kafkaproducer.NewKafkaProducer(producer, logger, commitCh, kafkaTopic)
+	kafkaProducer, err := kafkaproducer.NewKafkaProducer(producer, logger, commitCh, cfg.Kafka.Topic)
 	if err != nil {
+		logger.Error("Failed to create a new Kafka producer", slog.String("error", err.Error()))
 		return nil, err
 	}
 
-	closer.Add(func() error {
+	logger.Info("Successfully created a Kafka producer")
+
+	closer.Add(func(ctx context.Context) error {
+		logger.Info("Stopping Kafka Producer...")
 		kafkaProducer.Stop()
 		return nil
 	})
 
-	hub := hub.NewHub(gitHubClient, commitCh, slog.Default())
+	hub := hub.NewHub(gitHubClient, commitCh, logger)
 
-	closer.Add(func() error {
+	closer.Add(func(ctx context.Context) error {
+		logger.Info("Stopping Hub...")
 		hub.Stop()
 		return nil
 	})
 
-	usecase , err := service.NewService(nil, hub, logger)
+	txManager := postgres.NewTxManager(pool, logger)
+
+	postgresRepo := postgres.NewPostgresRepository(txManager, logger)
+
+	usecase , err := service.NewService(postgresRepo, txManager, hub, logger)
 	if err != nil {
+		logger.Error("Failed to create a new service", slog.String("error", err.Error()))
 		return nil, err
 	}
 
@@ -113,9 +141,7 @@ func (a *App) Run() error {
 
 	a.logger.Info("Запуск gRPC сервера", "grpcAddr", grpcAddr)
 
-	errorCh := make(chan error, 1)
-
-	errorChProxy := make(chan error, 1)
+	errorCh := make(chan error, 2) 
 
 	a.hub.Run()
 
@@ -128,11 +154,10 @@ func (a *App) Run() error {
 	}()
 
 	go func() {
-		if err := gateway.RunGateway(context.Background(), grpcAddr, httpAddr); err != nil {
-			errorChProxy <- fmt.Errorf("http proxy server error: %v", err)
+		if err := gateway.RunGateway(context.Background(), grpcAddr, httpAddr, a.logger); err != nil {
+			errorCh <- fmt.Errorf("http proxy server error: %v", err)
 		}
 	}()
-
 
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
@@ -140,7 +165,7 @@ func (a *App) Run() error {
 	select {
 	case <-quit:
 		a.logger.Info("Получен сигнал завершения, выключаем gRPC сервер...")
-		if err := a.closer.Close(); err != nil {
+		if err := a.closer.Close(context.Background()); err != nil {
 			a.logger.Error("Ошибка при закрытии ресурсов", slog.String("error", err.Error()))
 		} else {
 			a.logger.Info("Все ресурсы корректно закрыты")
@@ -149,13 +174,7 @@ func (a *App) Run() error {
 		return nil
 	case err := <-errorCh:
 		a.logger.Error("Ошибка сервера", slog.String("error", err.Error()))
-		if err := a.closer.Close(); err != nil {
-			a.logger.Error("Ошибка при закрытии ресурсов", slog.String("error", err.Error()))
-		}
-		return err
-	case err := <-errorChProxy:
-		a.logger.Error("Ошибка прокси-сервера", slog.String("error", err.Error()))
-		if err := a.closer.Close(); err != nil {
+		if err := a.closer.Close(context.Background()); err != nil {
 			a.logger.Error("Ошибка при закрытии ресурсов", slog.String("error", err.Error()))
 		}
 		return err
