9244a23f48434670b868ca4a7d7a61c351b82f8c
anton <antonfedorov190@yandex.ru>
2025-03-16 19:51:37 +0300

    chore(testing): consolidate test into a single file

--- scrapper/internal/hub/hub_add_link_test.go
+++ scrapper/internal/hub/hub_add_link_test.go
@@ -5,131 +5,13 @@ import (
 	"testing"
 	"time"
 
-	mockgithub "tbank/scrapper/internal/mocks/github"
+	mockgithub "tbank/scrapper/pkg/github_client/mock"
 
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-github/v69/github"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
-
-func TestHub_AddLink_SendsCommit(t *testing.T) {
-
-	commitChan := make(chan CustomCommit)
-
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	mockClient := mockgithub.NewMockGitHubClient(ctrl)
-
-	url := "https://github.com/Ranik23/tbank-tech"
-
-	mockClient.EXPECT().LatestCommit(gomock.Any(), "Ranik23", "tbank-tech", gomock.Any()).Return(
-		&github.RepositoryCommit{
-			SHA: github.Ptr("test_sha"),
-			Commit: &github.Commit{
-				Message: github.Ptr("Test commit message"),
-			},
-		}, nil, nil,
-	).AnyTimes()
-
-	hub := NewHub(mockClient, commitChan, slog.Default())
-
-	hub.AddLink(url, 1)
-
-
-	select {
-	case commit := <-commitChan:
-		assert.Equal(t, "test_sha", *commit.Commit.SHA)
-		assert.Equal(t, "Test commit message", *commit.Commit.Commit.Message)
-	case <-time.After(5 * time.Second):
-		t.Fatalf("Expected commit, but got none")
-	}
-}
-
-
-func TestHub_AddLinks_SendsCommits(t *testing.T) {
-
-	commitChan := make(chan CustomCommit)
-
-	ctrl := gomock.NewController(t)
-
-	mockClient := mockgithub.NewMockGitHubClient(ctrl)
-
-	url := "https://github.com/Ranik23/tbank-tech"
-
-	mockClient.EXPECT().LatestCommit(gomock.Any(), "Ranik23", "tbank-tech", gomock.Any()).Return(
-		&github.RepositoryCommit{
-			SHA: github.Ptr("test_sha"),
-			Commit: &github.Commit{
-				Message: github.Ptr("Test commit message"),
-			},
-		}, nil, nil,
-	).AnyTimes()
-
-	url2 := "https://github.com/Ranik23/weather-api-swagger"
-
-	mockClient.EXPECT().LatestCommit(gomock.Any(), "Ranik23", "weather-api-swagger", gomock.Any()).Return(
-		&github.RepositoryCommit{
-			SHA: github.Ptr("test_sha2"),
-			Commit: &github.Commit{
-				Message: github.Ptr("Test commit message2"),
-			},
-		}, nil, nil,
-	).AnyTimes()
-
-	hub := NewHub(mockClient, commitChan, slog.Default())
-
-	hub.AddLink(url, 1)
-	hub.AddLink(url2, 2)
-
-	count := 0
-	expectedCount := 2
-	
-	outerLoop:
-	for {
-		select {
-		case repoCommit := <-commitChan:
-			t.Logf("SHA - %s, Message - %s", *repoCommit.Commit.SHA, *repoCommit.Commit.Commit.Message)
-			count++
-			if count == expectedCount {
-				return
-			}
-		case <-time.After(5 * time.Second):
-			break outerLoop
-		}
-	}
-	
-	assert.Equal(t, count, expectedCount)
-}
-
-
-func TestHub_AddLink_ErrorFetchingCommit(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	mockClient := mockgithub.NewMockGitHubClient(ctrl)
-	commitChan := make(chan CustomCommit)
-	logger := slog.Default()
-
-	hub := NewHub(mockClient, commitChan, logger)
-
-	url := "https://github.com/Ranik23/tbank-tech"
-
-	mockClient.EXPECT().LatestCommit(gomock.Any(), "Ranik23", "tbank-tech", gomock.Any()).Return(
-		nil, nil, assert.AnError,
-	).AnyTimes()
-
-	hub.AddLink(url, 1)
-
-	time.Sleep(2 * time.Second)
-
-	select {
-	case <-commitChan:
-		t.Fatalf("Expected no commit, but got one")
-	default:
-
-	}
-}
 
 
 func TestHub_AddLink_UpdatesCommitOnNewSHA(t *testing.T) {
@@ -163,8 +45,10 @@ func TestHub_AddLink_UpdatesCommitOnNewSHA(t *testing.T) {
 		}, nil, nil,
 	).Times(1)
 
-	hub.AddLink(url, 1)
+	hub.Run()
 
+	err := hub.AddLink(url, 1); 
+	require.NoError(t, err)
 
 	select {
 	case commit := <-commitChan:
@@ -181,4 +65,134 @@ func TestHub_AddLink_UpdatesCommitOnNewSHA(t *testing.T) {
 	case <-time.After(5 * time.Second):
 		t.Fatalf("Expected second commit, but got none")
 	}
+
+	hub.Stop()
+}
+
+func TestHub_AddLinks_SendsCommitsMultiple(t *testing.T) {
+
+	commitChan := make(chan CustomCommit)
+
+	ctrl := gomock.NewController(t)
+
+	mockClient := mockgithub.NewMockGitHubClient(ctrl)
+
+	url := "https://github.com/Ranik23/tbank-tech"
+
+	mockClient.EXPECT().LatestCommit(gomock.Any(), "Ranik23", "tbank-tech", gomock.Any()).Return(
+		&github.RepositoryCommit{
+			SHA: github.Ptr("test_sha"),
+			Commit: &github.Commit{
+				Message: github.Ptr("Test commit message"),
+			},
+		}, nil, nil,
+	).AnyTimes()
+
+	url2 := "https://github.com/Ranik23/weather-api-swagger"
+
+	mockClient.EXPECT().LatestCommit(gomock.Any(), "Ranik23", "weather-api-swagger", gomock.Any()).Return(
+		&github.RepositoryCommit{
+			SHA: github.Ptr("test_sha2"),
+			Commit: &github.Commit{
+				Message: github.Ptr("Test commit message2"),
+			},
+		}, nil, nil,
+	).AnyTimes()
+
+	hub := NewHub(mockClient, commitChan, slog.Default())
+
+	hub.Run()
+
+	hub.AddLink(url, 1)
+	hub.AddLink(url2, 2)
+
+	count := 0
+	expectedCount := 2
+	
+	outerLoop:
+	for {
+		select {
+		case repoCommit := <-commitChan:
+			t.Logf("SHA - %s, Message - %s", *repoCommit.Commit.SHA, *repoCommit.Commit.Commit.Message)
+			count++
+			if count == expectedCount {
+				return
+			}
+		case <-time.After(5 * time.Second):
+			break outerLoop
+		}
+	}
+	
+	assert.Equal(t, count, expectedCount)
+
+	hub.Stop()
+}
+
+func TestHub_AddLink_SendsCommit(t *testing.T) {
+
+	commitChan := make(chan CustomCommit)
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockClient := mockgithub.NewMockGitHubClient(ctrl)
+
+	url := "https://github.com/Ranik23/tbank-tech"
+
+	mockClient.EXPECT().LatestCommit(gomock.Any(), "Ranik23", "tbank-tech", gomock.Any()).Return(
+		&github.RepositoryCommit{
+			SHA: github.Ptr("test_sha"),
+			Commit: &github.Commit{
+				Message: github.Ptr("Test commit message"),
+			},
+		}, nil, nil,
+	).AnyTimes()
+
+	hub := NewHub(mockClient, commitChan, slog.Default())
+
+	hub.Run()
+
+	hub.AddLink(url, 1)
+
+
+	select {
+	case commit := <-commitChan:
+		assert.Equal(t, "test_sha", *commit.Commit.SHA)
+		assert.Equal(t, "Test commit message", *commit.Commit.Commit.Message)
+	case <-time.After(5 * time.Second):
+		t.Fatalf("Expected commit, but got none")
+	}
+
+	hub.Stop()
+}
+
+func TestHub_AddLink_ErrorFetchingCommit(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockClient := mockgithub.NewMockGitHubClient(ctrl)
+	commitChan := make(chan CustomCommit)
+	logger := slog.Default()
+
+	hub := NewHub(mockClient, commitChan, logger)
+
+	url := "https://github.com/Ranik23/tbank-tech"
+
+	mockClient.EXPECT().LatestCommit(gomock.Any(), "Ranik23", "tbank-tech", gomock.Any()).Return(
+		nil, nil, assert.AnError,
+	).AnyTimes()
+
+	hub.Run()
+
+	hub.AddLink(url, 1)
+
+	time.Sleep(2 * time.Second)
+
+	select {
+	case <-commitChan:
+		t.Fatalf("Expected no commit, but got one")
+	default:
+	}
+
+	hub.Stop()
 }
