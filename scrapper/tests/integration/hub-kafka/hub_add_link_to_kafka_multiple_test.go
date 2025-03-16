8755e53491839b52d85f5ac3389fc95f1414c669
anton <antonfedorov190@yandex.ru>
2025-03-16 20:03:08 +0300

    feat(testing): integration test add

---
+++ scrapper/tests/integration/hub-kafka/hub_add_link_to_kafka_multiple_test.go
@@ -0,0 +1,170 @@
+package hubkafka
+
+import (
+	"encoding/json"
+	"log/slog"
+	"tbank/scrapper/internal/hub"
+	kafkaproducer "tbank/scrapper/internal/kafka_producer"
+	git "tbank/scrapper/pkg/github_client/mock"
+	"testing"
+	"github.com/IBM/sarama"
+	"github.com/golang/mock/gomock"
+	"github.com/google/go-github/v69/github"
+)
+
+func TestHub_AddLink_SendsCommitToKafkaAndReceivesItMultiple(t *testing.T) {
+
+//											SETTING UP AND STARTING HUB AND KAFKA CONSUMER											//
+//----------------------------------------------------------------------------------------------------------------------------------//
+
+	addresses := []string{"localhost:9093"}
+	linkExample := "https://github.com/Ranik23/tbank-tech"
+	exampleTopic := "test_topic"
+
+	commitExample1 := &github.RepositoryCommit{
+		SHA: github.Ptr("test_sha"),
+		Commit: &github.Commit{
+			Message: github.Ptr("test_message"),
+		},
+	}
+
+	commitExample2 := &github.RepositoryCommit{
+		SHA: github.Ptr("test_sha2"),
+		Commit: &github.Commit{
+			Message: github.Ptr("test_message2"),
+		},
+	}
+
+	commitExample3 := &github.RepositoryCommit{
+		SHA: github.Ptr("test_sha3"),
+		Commit: &github.Commit{
+			Message: github.Ptr("test_message3"),
+		},
+	}
+
+	exampleCommits := []*github.RepositoryCommit{
+		commitExample1,
+		commitExample2,
+		commitExample3,
+	}
+
+	logger := slog.Default()
+
+	controller := gomock.NewController(t)
+
+	mockGit := git.NewMockGitHubClient(controller)
+
+	call1 := mockGit.EXPECT().LatestCommit(gomock.Any(), "Ranik23", "tbank-tech", gomock.Any()).Return(
+		commitExample1, nil, nil,
+	).Times(1)
+
+	call2 := mockGit.EXPECT().LatestCommit(gomock.Any(), "Ranik23", "tbank-tech", gomock.Any()).Return(
+		commitExample2, nil, nil,
+	).Times(1).After(call1)
+
+	mockGit.EXPECT().LatestCommit(gomock.Any(), "Ranik23", "tbank-tech", gomock.Any()).Return(
+		commitExample3, nil, nil,
+	).AnyTimes().After(call2)
+
+
+	commitCh := make(chan hub.CustomCommit)
+
+	myHub := hub.NewHub(mockGit, commitCh, logger)
+
+	saramaConfig := sarama.NewConfig()
+	saramaConfig.Producer.Return.Successes = true
+	saramaConfig.Producer.Return.Errors = true
+
+	producer, err := sarama.NewAsyncProducer(addresses, saramaConfig)
+	if err != nil {
+		t.Fatalf("Failed to create a new async producer: %v", err)
+	}
+
+	kafkaProducer, err := kafkaproducer.NewKafkaProducer(producer, logger, commitCh, exampleTopic)
+	if err != nil {
+		t.Fatalf("Failed to create a new kafka: %v", err)
+	}
+
+	kafkaProducer.Run()
+	defer kafkaProducer.Stop()
+
+	myHub.Run()
+	defer myHub.Stop()
+
+	myHub.AddLink(linkExample, 1)
+
+
+//															CONSUMER STARTING                                                       // 
+//----------------------------------------------------------------------------------------------------------------------------------//
+
+	kafkaConsumer, err := sarama.NewConsumer(addresses, saramaConfig)
+	if err != nil {
+		t.Fatalf("Failed to create a new consumer: %v", err)
+	}
+	defer kafkaConsumer.Close()
+
+	partitions, err := kafkaConsumer.Partitions(exampleTopic)
+	if err != nil {
+		t.Fatalf("Failed to list the partitions for the given topic: %v", err)
+	}
+
+	if len(partitions) == 0 {
+		t.Fatalf("Partitions number should be > 0")
+	}
+
+	t.Logf("Partitions - %d", len(partitions))
+
+	theOnlyPartition := partitions[0]
+
+	partitionConsumer, err := kafkaConsumer.ConsumePartition(exampleTopic, theOnlyPartition, sarama.OffsetNewest)
+	if err != nil {
+		t.Fatalf("Failed to create a consumer on this specific partition: %v", err)
+	}
+	defer partitionConsumer.Close()
+
+	errorCh := make(chan error)
+	receivedCommitCh := make(chan hub.CustomCommit)
+
+	go func() {
+		for message := range partitionConsumer.Messages() {
+			var receivedCommit hub.CustomCommit
+			if err := json.Unmarshal(message.Value, &receivedCommit); err != nil {
+				errorCh <- err
+				return
+			}
+			logger.Info("Message sent to receivedChannel")
+			receivedCommitCh <- receivedCommit
+		}
+	}()
+
+	i := 0
+
+Loop:
+	for i <= 2 {
+		select {
+		case err := <-errorCh:
+			t.Fatalf("Error occurred: %v", err)
+		case commit := <-receivedCommitCh:
+			if *commit.Commit.SHA != *exampleCommits[i].SHA ||
+				*commit.Commit.Commit.Message != *exampleCommits[i].Commit.Message {
+				t.Fatalf("Commit Message is not the same. Expected: %s %s, Got: %s %s",
+					*exampleCommits[i].SHA, *exampleCommits[i].Commit.Message,
+					*commit.Commit.SHA, *commit.Commit.Commit.Message)
+			}
+
+			t.Logf("Expected SHA - %s, Real SHA - %s, Expected Message - %s, Real Message - %s",
+				*exampleCommits[i].SHA, *commit.Commit.SHA,
+				*exampleCommits[i].Commit.Message, *commit.Commit.Commit.Message)
+
+			if i == 2 {
+				break Loop
+			}
+
+			i++
+		}
+	}
+
+	close(errorCh)
+	close(receivedCommitCh)
+
+}
---
+++ scrapper/tests/integration/usecase-storage/storage_test.go
@@ -0,0 +1,31 @@
+package usecasestorage
+
+import (
+	// "context"
+	// "fmt"
+	// "tbank/scrapper/internal/repository/postgres"
+	// "tbank/scrapper/internal/service"
+	"testing"
+
+	// "github.com/jackc/pgx/v5/pgxpool"
+)
+
+
+
+
+func Test(t *testing.T) {
+
+
+	// dsn := fmt.Sprintf()
+
+	// pool := pgxpool.New(context.Background(), )
+
+	// txManager := postgres.NewTxManager()
+
+	// repo := postgres.NewPostgresRepository()
+
+	// service := service.NewService()
+
+}
+
+
