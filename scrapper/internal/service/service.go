4f3bfb0d921bf8dc26c202732989d05f858677ec
anton <antonfedorov190@yandex.ru>
2025-03-16 20:00:14 +0300

    chore: various improvements and fixes

--- scrapper/internal/service/service.go
+++ scrapper/internal/service/service.go
@@ -9,137 +9,100 @@ import (
 	"tbank/scrapper/internal/repository"
 )
 
-
 var (
 	ErrEmptyLink = fmt.Errorf("empty link")
 	EmptyLinkURL = ""
 )
 
 type Service interface {
-	RegisterUser(ctx context.Context, userID uint, name string) 										error
-	DeleteUser(ctx context.Context, userID uint) 														error
-	GetLinks(ctx context.Context, userID uint) 															([]dbmodels.Link, error)
-	AddLink(ctx context.Context, link dbmodels.Link, userID uint) 										(*dbmodels.Link, error)
-	RemoveLink(ctx context.Context, link dbmodels.Link, userID uint) 									error
+	RegisterUser(ctx context.Context, userID uint, name string) error
+	DeleteUser(ctx context.Context, userID uint) error
+	GetLinks(ctx context.Context, userID uint) ([]dbmodels.Link, error)
+	AddLink(ctx context.Context, link dbmodels.Link, userID uint) (*dbmodels.Link, error)
+	RemoveLink(ctx context.Context, link dbmodels.Link, userID uint) error
 }
 
 type service struct {
-	logger 		*slog.Logger
-	hub			hub.Hub
-	repo		repository.Repository
+	txManager repository.TxManager
+	logger   *slog.Logger
+	hub      hub.Hub
+	repo     repository.Repository
 }
 
-func NewService(repo repository.Repository, hub hub.Hub, logger *slog.Logger) (Service, error) {
+func NewService(repo repository.Repository, txManager repository.TxManager, hub hub.Hub, logger *slog.Logger) (Service, error) {
 	return &service{
-		repo: repo,
-		hub: hub,
-		logger: logger,
+		repo:      repo,
+		txManager: txManager,
+		hub:       hub,
+		logger:    logger,
 	}, nil
 }
 
 func (s *service) RegisterUser(ctx context.Context, userID uint, name string) error {
-	tx, err := s.repo.BeginTx(ctx)
-	if err != nil {
-		return err
-	}
-
-	defer func() {
-		if err != nil {
-			if rollbackErr := tx.Rollback(ctx); rollbackErr != nil {
-				s.logger.Error("transaction rollback failed", "error", rollbackErr)
-				err = fmt.Errorf("original error: %w, rollback error: %w", err, rollbackErr)
-			}
-		}
-	}()
-
-	if err = s.repo.CreateUser(ctx, userID, name); err != nil {
-		return err
-	}
-
-	return tx.Commit(ctx)
+	return s.txManager.WithTx(ctx, func(txCtx context.Context) error {
+		return s.repo.CreateUser(txCtx, userID, name)
+	})
 }
 
 func (s *service) DeleteUser(ctx context.Context, userID uint) error {
-	return s.repo.DeleteUser(ctx, userID)
+	return s.txManager.WithTx(ctx, func(txCtx context.Context) error {
+		return s.repo.DeleteUser(txCtx, userID)
+	})
 }
 
 func (s *service) GetLinks(ctx context.Context, userID uint) ([]dbmodels.Link, error) {
-	return s.repo.GetURLS(ctx, userID)
+	var links []dbmodels.Link
+	err := s.txManager.WithTx(ctx, func(txCtx context.Context) error {
+		var err error
+		links, err = s.repo.GetURLS(txCtx, userID)
+		return err
+	})
+	return links, err
 }
 
 func (s *service) AddLink(ctx context.Context, link dbmodels.Link, userID uint) (*dbmodels.Link, error) {
-	tx, err := s.repo.BeginTx(ctx)
-	if err != nil {
-		return nil, err
-	}
+	var linkNew *dbmodels.Link
 
-	defer func() {
-		if err != nil {
-			if rollbackErr := tx.Rollback(ctx); rollbackErr != nil {
-				s.logger.Error("transaction rollback failed", "error", rollbackErr)
-				err = fmt.Errorf("original error: %w, rollback error: %w", err, rollbackErr)
-			}
+	err := s.txManager.WithTx(ctx, func(txCtx context.Context) error {
+		if link.Url == EmptyLinkURL {
+			return ErrEmptyLink
 		}
-	}()
 
-	if link.Url == EmptyLinkURL {
-		err = ErrEmptyLink
-		return nil, err
-	}
+		if err := s.hub.AddLink(link.Url, userID); err != nil {
+			return err
+		}
 
-	s.hub.AddLink(link.Url, userID)
+		if err := s.repo.CreateLink(txCtx, link.Url); err != nil {
+			return err
+		}
 
-	if err = s.repo.CreateLink(ctx, link.Url); err != nil {
-		return nil, err
-	}
+		var err error
+		linkNew, err = s.repo.GetLinkByURL(txCtx, link.Url)
+		if err != nil {
+			return err
+		}
 
-	linkNew, err := s.repo.GetLinkByURL(ctx, link.Url)
-	if err != nil {
-		return nil, err
-	}
+		return s.repo.CreateLinkUser(txCtx, linkNew.ID, userID)
+	})
 
-	if err = s.repo.CreateLinkUser(ctx, linkNew.ID, userID); err != nil {
-		return nil, err
-	}
-	
-	if err = tx.Commit(ctx); err != nil {
-		return nil, err
-	}
-
-	return linkNew, nil
+	return linkNew, err
 }
 
-
 func (s *service) RemoveLink(ctx context.Context, link dbmodels.Link, userID uint) error {
-	tx, err := s.repo.BeginTx(ctx)
-	if err != nil {
-		return err
-	}
-
-	defer func() {
-		if err != nil {
-			if rollbackErr := tx.Rollback(ctx); rollbackErr != nil {
-				s.logger.Error("transaction rollback failed", "error", rollbackErr)
-				err = fmt.Errorf("original error: %w, rollback error: %w", err, rollbackErr)
-			}
+	return s.txManager.WithTx(ctx, func(txCtx context.Context) error {
+		if link.Url == EmptyLinkURL {
+			return ErrEmptyLink
 		}
-	}()
 
-	if link.Url == "" {
-		err = ErrEmptyLink
-		return err
-	}
+		if err := s.hub.RemoveLink(link.Url, userID); err != nil {
+			return err
+		}
 
-	s.hub.RemoveLink(link.Url, userID)
+		linkNew, err := s.repo.GetLinkByURL(txCtx, link.Url)
+		if err != nil {
+			return err
+		}
 
-	linkNew, err := s.repo.GetLinkByURL(ctx, link.Url)
-	if err != nil {
-		return err
-	}
-
-	if err = s.repo.DeleteLink(ctx, linkNew.ID); err != nil {
-		return err
-	}
-	
-	return tx.Commit(ctx)
+		return s.repo.DeleteLink(txCtx, linkNew.ID)
+	})
 }
