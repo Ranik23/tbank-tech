64dcdc4a3a4145cfa5212a04e0d022b8a45e8292
anton <antonfedorov190@yandex.ru>
2025-03-21 20:34:24 +0300

    feat(gateway): add logging

--- bot/internal/gateway/gateway.go
+++ bot/internal/gateway/gateway.go
@@ -1,7 +1,9 @@
 package gateway
 
 import (
+	"bytes"
 	"context"
+	"io"
 	"log/slog"
 	"net/http"
 
@@ -10,8 +12,6 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
-
-
 
 func RunGateway(ctx context.Context, grpcAddr string, httpAddr string, logger *slog.Logger) error {
 	const op = "Gateway.RunGateway"
@@ -24,6 +24,18 @@ func RunGateway(ctx context.Context, grpcAddr string, httpAddr string, logger *s
 		return err
 	}
 
-	logger.Info(op, slog.String("message", "Starting proxy server"), slog.String("httpAddr", httpAddr))
-	return http.ListenAndServe(httpAddr, mux)
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			logger.Error(op, slog.String("message", "Failed to read request body"), slog.String("error", err.Error()))
+			http.Error(w, "Failed to read request body", http.StatusBadRequest)
+			return
+		}
+		r.Body = io.NopCloser(bytes.NewReader(body))
+		logger.Info(op, slog.String("incoming_request", string(body)))
+		mux.ServeHTTP(w, r)
+	}) 
+
+	logger.Info(op, slog.String("message", "Starting Bot Proxy Server"), slog.String("httpAddr", httpAddr))
+	return http.ListenAndServe(httpAddr, handler)
 }
